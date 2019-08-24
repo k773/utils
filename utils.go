883517fa4acfbbe26eeeca50cb725bf5c10a8ef9
k773 <sosy@gmail.com>
2019-08-24 19:05:37 +0300

    Added ban

--- utils.go
+++ utils.go
@@ -1,7 +1,70 @@
 package utils
 
-import "fmt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+)
 
-func ban() {
-	fmt.Println("1")
+func encryptBtB(strkey string, text []byte) []byte {
+	key, _ := hex.DecodeString(strkey)
+	plaintext := text
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext
+}
+
+func decryptBtB(strkey string, bytes []byte) []byte {
+	// Load your secret key from a safe place and reuse it across multiple
+	// NewCipher calls. (Obviously don't use this example key for anything
+	// real.) If you want to convert a passphrase to a key, use a suitable
+	// package like bcrypt or scrypt.
+	key, _ := hex.DecodeString(strkey)
+	ciphertext := bytes
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertext, ciphertext)
+	return ciphertext
+}
+
+func encryptStH(strkey string, str string) string {
+	return hex.EncodeToString(encryptBtB(strkey, []byte(str)))
+}
+
+func decryptHtS(strkey string, hexStr string) string {
+	ciphertext, _ := hex.DecodeString(hexStr)
+	return string(decryptBtB(strkey, ciphertext))
+}
+
+func sha256StH(text string) string {
+	h := sha256.New()
+	h.Write([]byte(text))
+	return hex.EncodeToString(h.Sum(nil))
 }
