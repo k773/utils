8a8b64b9f9c6d2d61e8f0c57649ffba3d63fcf00
k773 <lollinux_borealis@outlook.com>
2021-04-02 19:16:20 +0300

    New features(new bugs) by k773

--- counters.go
+++ counters.go
@@ -1,29 +1,106 @@
 package utils
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type SafeCounter struct {
+	sync.RWMutex
+
+	waiters struct {
+		sync.RWMutex
+		m map[int64]*chan int
+	}
 	Value int
-	s     sync.Mutex
+}
+
+func NewSafeCounter() SafeCounter {
+	return SafeCounter{
+		RWMutex: sync.RWMutex{},
+		waiters: struct {
+			sync.RWMutex
+			m map[int64]*chan int
+		}{m: map[int64]*chan int{}},
+		Value: 0,
+	}
 }
 
 func (c *SafeCounter) Increase() {
-	c.s.Lock()
+	c.Lock()
+	defer c.Unlock()
 	c.Value++
-	c.s.Unlock()
+	c.notify(c.Value)
 }
 
 func (c *SafeCounter) Decrease() {
-	c.s.Lock()
+	c.Lock()
+	defer c.Unlock()
 	c.Value--
-	c.s.Unlock()
+	c.notify(c.Value)
+}
+
+func (c *SafeCounter) Get() int {
+	c.RLock()
+	defer c.RUnlock()
+	return c.Value
+}
+
+func (c *SafeCounter) notify(num int) {
+	c.waiters.RLock()
+	for _, waiter := range c.waiters.m {
+		*waiter <- num
+	}
+	c.waiters.RUnlock()
+}
+
+// param t: 0 - will return if value less or equals i, 1 - if value equals i, 2 - if value greater or equals i
+type waitBehaviour int
+
+const (
+	WaitBehaviourLessOrEquals    waitBehaviour = 0
+	WaitBehaviourEquals          waitBehaviour = 1
+	WaitBehaviourGreaterOrEquals waitBehaviour = 2
+)
+
+func (c *SafeCounter) Wait(i int, behaviour waitBehaviour) {
+	waiterKey := time.Now().UnixNano()
+	ch := make(chan int, 1)
+	ch <- c.Get()
+
+	c.waiters.Lock()
+	c.waiters.m[waiterKey] = &ch
+	c.waiters.Unlock()
+
+f:
+	for {
+		v := <-ch
+		switch behaviour {
+		case WaitBehaviourEquals:
+			if v == i {
+				break f
+			}
+		case WaitBehaviourLessOrEquals:
+			if v <= i {
+				break f
+			}
+		case WaitBehaviourGreaterOrEquals:
+			if v >= i {
+				break f
+			}
+		}
+	}
+
+	c.waiters.Lock()
+	delete(c.waiters.m, waiterKey)
+	c.waiters.Unlock()
 }
 
 type SafeCounterLimited struct {
 	MaxValue int
 	DefValue int
 	Value    int
-	s        sync.Mutex
+	s        sync.RWMutex
 }
 
 func (c *SafeCounterLimited) Increase() {
@@ -40,6 +117,12 @@ func (c *SafeCounterLimited) Decrease() {
 	c.s.Lock()
 	c.Value--
 	c.s.Unlock()
+}
+
+func (c *SafeCounterLimited) Get() int {
+	c.s.RLock()
+	defer c.s.RUnlock()
+	return c.Value
 }
 
 //F
