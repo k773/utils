af96ee9aa9a6508e32794fc8334ef6178593f6d7
k773 <lollinux_borealis@outlook.com>
2022-12-09 01:33:55 +0300

    update

--- loop.go
+++ loop.go
@@ -23,13 +23,16 @@ func RunForeverFirstSyncThenAsync(ctx context.Context, f func(), every time.Dura
 
 	go func() {
 		var t = time.NewTicker(every)
-		for range t.C {
-			if ctx.Err() != nil {
-				break
+		defer t.Stop()
+	l:
+		for {
+			select {
+			case <-ctx.Done():
+				break l
+			case <-t.C:
+				run()
 			}
-			run()
 		}
-		t.Stop()
 	}()
 }
 
@@ -45,13 +48,16 @@ func RunForeverAsyncNoFirstTime(ctx context.Context, f func(), every time.Durati
 	if ctx.Err() == nil {
 		go func() {
 			var t = time.NewTicker(every)
-			for range t.C {
-				if ctx.Err() != nil {
-					break
+			defer t.Stop()
+		l:
+			for {
+				select {
+				case <-ctx.Done():
+					break l
+				case <-t.C:
+					run()
 				}
-				run()
 			}
-			t.Stop()
 		}()
 	}
 }
@@ -72,13 +78,16 @@ func RunForeverSync(ctx context.Context, f func(), every time.Duration, simultan
 	}
 
 	var t = time.NewTicker(every)
-	for range t.C {
-		if ctx.Err() != nil {
-			break
+	defer t.Stop()
+l:
+	for {
+		select {
+		case <-ctx.Done():
+			break l
+		case <-t.C:
+			run()
 		}
-		run()
 	}
-	t.Stop()
 }
 
 // RunForeverSyncUntil : f func should return false to stop the execution
@@ -88,12 +97,18 @@ func RunForeverSyncUntil(ctx context.Context, f func() bool, every time.Duration
 	}
 
 	var t = time.NewTicker(every)
-	for range t.C {
-		if ctx.Err() != nil || !f() {
-			break
+	defer t.Stop()
+l:
+	for {
+		select {
+		case <-ctx.Done():
+			break l
+		case <-t.C:
+			if !f() {
+				break l
+			}
 		}
 	}
-	t.Stop()
 }
 
 // DelayedExecution will trigger wg only at the end of the execution; increasing the value before calling this function is up to the caller
