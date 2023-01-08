c3a09be88995d091488cfde9e64a1ce9976ea71a
k773 <lollinux_borealis@outlook.com>
2023-01-08 16:30:32 +0300

    map tools

--- maps/tools.go
+++ maps/tools.go
@@ -8,3 +8,39 @@ func CloneAndClear[T ~map[K]V, K comparable, V any](m T) map[K]V {
 	}
 	return res
 }
+
+func ValuesAndClear[T ~map[K]V, K comparable, V any](m T) []V {
+	var res = make([]V, len(m))
+	var i int
+	for k, v := range m {
+		delete(m, k)
+		res[i] = v
+		i++
+	}
+	return res
+}
+
+func KeysAndClear[T ~map[K]V, K comparable, V any](m T) []K {
+	var res = make([]K, len(m))
+	var i int
+	for k := range m {
+		delete(m, k)
+		res[i] = k
+		i++
+	}
+	return res
+}
+
+func Clear[T ~map[K]V, K comparable, V any](m T) {
+	for k := range m {
+		delete(m, k)
+	}
+}
+
+func Clone[T ~map[K]V, K comparable, V any](m T) map[K]V {
+	var res = make(map[K]V, len(m))
+	for k, v := range m {
+		res[k] = v
+	}
+	return res
+}
