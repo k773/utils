ee30733a472dad6aec4ed9d7df3918eb91a9428d
k773 <lollinux_borealis@outlook.com>
2023-05-25 14:00:48 +0300

    rand: generating random usernames by retrieving data from steam

--- rand.go
+++ rand.go
@@ -1,7 +1,13 @@
 package utils
 
 import (
+	cryptoRand "crypto/rand"
+	"errors"
+	"github.com/go-resty/resty/v2"
 	"io"
+	"regexp"
+	"strconv"
+	"unicode"
 	"unsafe"
 )
 
@@ -73,4 +79,102 @@ func RandValue[T any](reader io.Reader) (T, error) {
 	_, e := reader.Read(tmp)
 
 	return v, e
+}
+
+func RandIntN(reader io.Reader, n int64) (value int64, e error) {
+	value, e = RandValue[int64](reader)
+	if value < 0 {
+		value = -value
+	}
+	value = value % n
+	return
+}
+
+/*
+	Data gens
+*/
+
+var steamActualPersonaNameRe = regexp.MustCompile(`(?s)<span class="actual_persona_name">(.*?)</span>`)
+var randomUsernameSes = resty.New()
+
+// RandomUsername retrieves a random username from steam persona name (not using steam's username for that because too little people have it).
+// According to my tests, steam doesn't mind us sending tons of requests per second, so no need to limit the rps here.
+// Tested on maxAttempts = 10, with 100_000 runs none has failed. Average requests made per username = 1.52.
+func RandomUsername(maxAttempts int) (username string, attemptsUsed int, e error) {
+	for attemptsUsed = 1; attemptsUsed <= maxAttempts; attemptsUsed++ {
+		// Generating a random steam id.
+		// Since we're generating from a pretty big range of possible ids, it's conceivable that an invalid ID may be produced on rare occasions.
+		var randPart int64
+		randPart, e = RandIntN(cryptoRand.Reader, 400_000_000)
+		if e != nil {
+			continue
+		}
+		var id = 76561197960265728 + 50_000_000 + randPart
+
+		var r *resty.Response
+		r, e = randomUsernameSes.R().Get("https://steamcommunity.com/profiles/" + strconv.Itoa(int(id)))
+
+		//var randPart int64
+		//randPart, e = RandIntN(cryptoRand.Reader, 1_062_000_000)
+		//if e != nil {
+		//	continue
+		//}
+		//
+		//var r *resty.Response
+		//r, e = randomUsernameSes.R().Get(fmt.Sprintf("https://steamcommunity.com/profiles/[U:1:%v]", randPart))
+		if e == nil {
+			// Not, that steam returns 200 OK even if the profile doesn't exist
+			if !r.IsSuccess() {
+				e = errors.New(r.Status())
+				continue
+			}
+			found := steamActualPersonaNameRe.FindStringSubmatch(r.String())
+			if len(found) == 0 {
+				e = errors.New("regexp entries not found")
+				continue
+			}
+
+			// Replacing bad chars
+			var usernameRunes = []rune(found[1])
+			var onlyNumbers = true
+			for i, ch := range usernameRunes {
+				if !(unicode.Is(unicode.Latin, ch) || unicode.IsDigit(ch) || ch == ' ' || ch == '.' || ch == '_') {
+					e = errors.New("symbol not allowed")
+					break
+				}
+				onlyNumbers = onlyNumbers && unicode.IsDigit(ch)
+
+				if ch == ' ' {
+					ch, e = RandomChoice(cryptoRand.Reader, []rune{'_', '.'})
+					if e != nil {
+						break
+					}
+					usernameRunes[i] = ch
+				}
+			}
+			if e != nil {
+				continue
+			}
+			// Skipping only-numeric usernames
+			if onlyNumbers {
+				e = errors.New("username contains only numbers")
+				continue
+			}
+			// Skipping too short usernames
+			if len(usernameRunes) < 6 {
+				e = errors.New("too short")
+				continue
+			}
+			// Skipping with bad prefixes
+			if usernameRunes[0] == '.' || usernameRunes[0] == '_' {
+				e = errors.New("first symbol is not allowed")
+				continue
+			}
+
+			// Username satisfies all requirements
+			username = string(usernameRunes)
+			break
+		}
+	}
+	return
 }
