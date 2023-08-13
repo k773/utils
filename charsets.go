add72eca91f5ca8ffd0d015ba0a45baa6b4ea8da
k773 <lollinux_borealis@outlook.com>
2023-08-13 17:48:28 +0300

    upd

---
+++ charsets.go
@@ -0,0 +1,6 @@
+package utils
+
+var Numbers = []rune("1234567890")
+var LettersLowercase = []rune("abcdefghijklmnopqrstuvwxyz")
+var LettersUppercase = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var SpecialChars = []rune("!@#$%^&*()_+-=`~';:\"[{]},.<>?/")
