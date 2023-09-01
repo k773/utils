f215601790a6cc06212baa7e465baacd91ada201
k773 <lollinux_borealis@outlook.com>
2023-09-01 23:28:49 +0300

    upd

---
+++ slices.go
@@ -0,0 +1,17 @@
+package utils
+
+/*
+	Slice length reported
+*/
+
+type SliceLengthReporterInterface interface {
+	Len() int
+}
+
+type SliceLengthReporter[T any] struct {
+	Slice []T
+}
+
+func (s *SliceLengthReporter[T]) Len() int {
+	return len(s.Slice)
+}
