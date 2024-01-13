65b41b5cb11dd69225dc8aa16b5de33af530bd03
k773 <lollinux_borealis@outlook.com>
2024-01-13 20:59:02 +0300

    utils

--- slices.go
+++ slices.go
@@ -15,3 +15,13 @@ type SliceLengthReporter[T any] struct {
 func (s *SliceLengthReporter[T]) Len() int {
 	return len(*s.Slice)
 }
+
+func SliceFilter[T any](in []T, keep func(T) bool) []T {
+	var res []T
+	for _, v := range in {
+		if keep(v) {
+			res = append(res, v)
+		}
+	}
+	return res
+}
